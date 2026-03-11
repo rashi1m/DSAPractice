b8df8d24a8fb0c3041633526b8f1555bf857b110
rashi1m <rashipanchal2003@gmail.com>
2026-03-11 18:45:14 +0530

    convert float value into int type of value

--- src/coreJava/Primitive.java
+++ src/coreJava/Primitive.java
@@ -6,13 +6,18 @@ public class Primitive {
  
 	public static void main(String[] args) {
 		//type casting -> covert data type into another data type
-		Scanner sc=new Scanner(System.in);
-		float num1=sc.nextFloat();
-		float num2=sc.nextFloat();
+	Scanner sc=new Scanner(System.in);
+//		float num1=sc.nextFloat();
+//		float num2=sc.nextFloat();
+//		
+//		//if i will take num1=20 so it will automatically convert into float
+//		
+//		float sum= num1+num2;
+		//System.out.println("the sum is "+sum);
+	
+		int num3 =(int) 43.5f;
+		System.out.println(num3);
 		
-		//if i will take num1=20 so it will automatically convert into float
 		
-		float sum= num1+num2;
-		System.out.println("the sum is "+sum);
 	}
 }
