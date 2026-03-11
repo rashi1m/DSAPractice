4950088963bfa29bd8a227dd1f3a6dbd26e313f2
rashi1m <rashipanchal2003@gmail.com>
2026-03-11 15:17:48 +0530

    Scanner and Nextline method

--- src/coreJava/Class1Tutorial.java
+++ src/coreJava/Class1Tutorial.java
@@ -1,17 +1,23 @@
 package coreJava;
 
+import java.util.Scanner;
+
 public class Class1Tutorial {
 
 	public static void main(String[] args) {
 
-		int value = 16;
-		for(int v=2; v<value ;v++) {
-			if (value % v == 0) {
-				System.out.println("Number is not prime");
-			   return;
-			}
-		}
-		System.out.println("NUmber is prime");
+//		int value = 1;
+//		for(int v=2; v<value ;v++) {
+//			if (value % v == 0) {
+//				System.out.println("Number is not prime");
+//			   return;
+//			}
+//		}
+//		System.out.println("Number is prime");
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println(sc.nextLine());
+		
 	}
 	
 }
