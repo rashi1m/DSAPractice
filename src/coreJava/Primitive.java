80be979ceb27dd4b937f8241d10d0529e5286eb8
rashi1m <rashipanchal2003@gmail.com>
2026-03-11 18:37:09 +0530

    typecasting

--- src/coreJava/Primitive.java
+++ src/coreJava/Primitive.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Primitive {
  
 	public static void main(String[] args) {
-		
+		//type casting -> covert data type into another data type
 		Scanner sc=new Scanner(System.in);
 		float num1=sc.nextFloat();
 		float num2=sc.nextFloat();
+		
+		//if i will take num1=20 so it will automatically convert into float
 		
 		float sum= num1+num2;
 		System.out.println("the sum is "+sum);
