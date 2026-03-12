98babdc0435b724d556810692f0e724bbf88ed53
rashi1m <rashipanchal2003@gmail.com>
2026-03-12 20:38:00 +0530

    check alphabet is written in lowerCase or UpperCase

--- src/coreJava/CaseCheck.java
+++ src/coreJava/CaseCheck.java
@@ -8,6 +8,9 @@ public class CaseCheck {
 		Scanner in = new Scanner(System.in);
 		System.out.println("please enter alphabet here : ");
 		char check = in.next().trim().charAt(0);
+		
+		//next () gives a word and trim will remove extra space and charAt() which will show the on which alphabet
+		//is written on which index number
 		System.out.println(check);
 		
 		// it will check either alphabet is written in upperCase or lowerCase 
