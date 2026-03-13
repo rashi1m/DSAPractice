1ea45719022a602291c7b6b313aaf24ed0c3866e
rashi1m <rashipanchal2003@gmail.com>
2026-03-14 00:06:20 +0530

    reverse of a number

--- src/coreJava/CountingOccurences.java
+++ src/coreJava/CountingOccurences.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class CountingOccurences {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number here: ");
 int n= sc.nextInt();
 int count=0;
 
---
+++ src/coreJava/ReverseNumber.java
@@ -0,0 +1,20 @@
+package coreJava;
+import java.util.Scanner;
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number here : ");
+	int reverse = sc.nextInt();
+		
+	int answer = 0;
+	while(reverse>0)
+	{
+		int rem = reverse % 10;
+		reverse /= 10;
+		answer=answer*10+rem;
+		
+	}
+	System.out.println(answer);
+	}
+}
